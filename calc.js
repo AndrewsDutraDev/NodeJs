b9752d55dc9a878354a655992dd2ae9ce283cd66
Andrews <dutrapgmdr@gmail.com>
2020-03-06 09:14:04 -0300

    Aprendendo a exportar/importar módulos

---
+++ calc.js
@@ -0,0 +1,4 @@
+var SomaFunc = require("./somar") //require é usado para importar um módulo
+var SubFunc = require("./sub")
+console.log(SomaFunc(1,2))
+console.log(SubFunc(1,2))
---
+++ somar.js
@@ -0,0 +1,4 @@
+var soma = function(a,b){
+    return a+b
+}
+module.exports = soma // serve para exportar o modulo, deve sempre estar ligado a uma variavel
