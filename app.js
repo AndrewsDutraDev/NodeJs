9fff976a4a6a771faaf256e7a126a1b5f89f51e6
Andrews <dutrapgmdr@gmail.com>
2020-03-06 10:01:46 -0300

    Iniciando um servidor http

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log("Hello world")
+var http = require("http")
+http.createServer(function(req,res){
+    res.end("Hello World! Welcome to my WebSite") //enviar uma mensagem
+}).listen("8081")
+console.log("Servidor rodando")
